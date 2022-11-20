eb518dafc9ec48352e8416ab4ba890511c75c80b
yuragms <yura.gms@gmail.com>
2022-11-20 23:28:17 +0200

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { FavouritesPage } from './pages/FavouritesPage';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
+    <>
+    <Navigation/>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
-<Route path="/favourites" element={<FavouritesPage/>}/>
+      <Route path="/favourites" element={<FavouritesPage/>}/>
       
-    </Routes>
+      </Routes>
+      </>
   );
 }
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function Navigation() {
     return (
         <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
-           <h3>Github Search</h3> 
-        </nav>
+           <h3 >Github Search</h3> 
+       
+        <span>
+            <Link to="/" className="mr-2">Home</Link>
+              <Link to="/favourites">Favourites</Link>
+        </span>
+         </nav>
     )
 }
 
