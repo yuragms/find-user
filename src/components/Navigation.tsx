aff7efcb0a0570f69d2f575b22e53ef89e435d45
yuragms <yura.gms@gmail.com>
2022-11-21 15:39:27 +0200

    query

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 export function Navigation() {
     return (
         <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
-           <h3 >Github Search</h3> 
+           <h3 className="font-bold">Github Search</h3> 
        
         <span>
             <Link to="/" className="mr-2">Home</Link>
-              <Link to="/favourites">Favourites</Link>
+            <Link to="/favourites">Favourites</Link>
         </span>
          </nav>
     )
